566dbd06cc373b17356c88170b3c7874fcedd912
savakotsur <slendygood@gmail.com>
2025-11-27 15:32:07 +0300

    ci/cd fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,8 @@
-from src.main import add
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+from main import add
 
 def test_add():
     assert add(2, 3) == 5, "2 + 3 должно быть 5"
